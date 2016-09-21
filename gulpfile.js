58fb029dd73382b5c4aa5c0dbe98cf40ae039698
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-09-21 09:16:02 +0200

    Gulp eta bower gehitu

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync');
+
+var reload = browserSync.reload;
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+    browserSync({
+        server: {
+            baseDir: ''
+        }
+    });
+
+    gulp.watch(['index.html', 'css/**/*.css', 'js/**/*.js', 'csv/*.csv'], {cwd: ''}, reload);
+});
+
+gulp.task('kopiatuPublikora', function() {
+
+    console.log("Beharrezko JS eta CSS fitxategiak bower_components karpetatik public karpetara kopiatzen...");
+
+    // JS script fitxategiak kopiatu.
+    gulp.src(['bower_components/c3/c3.min.js',
+              'bower_components/d3/d3.min.js',
+              'bower_components/index.js',
+              'bower_components/topojson/topojson.js'], {
+                  base: 'bower_components'
+    }).pipe(gulp.dest('js'));
+
+    // CSS fitxategiak kopiatu.
+    gulp.src(['bower_components/c3/c3.min.css'], {
+                  base: 'bower_components'
+    }).pipe(gulp.dest('css'));
+});
+
+gulp.task('default', function() {
+    // place code for your default task here
+});
