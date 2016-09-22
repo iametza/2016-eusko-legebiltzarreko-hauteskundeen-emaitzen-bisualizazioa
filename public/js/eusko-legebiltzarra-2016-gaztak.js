65a3eb837a13e1bea3547a95d0923baca16f486b
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-09-22 11:22:46 +0200

    Gauge-ek ez digute balio, donut bat erabili

--- public/js/eusko-legebiltzarra-2016-gaztak.js
+++ public/js/eusko-legebiltzarra-2016-gaztak.js
@@ -5,41 +5,25 @@
     var chart = c3.generate({
         data: {
             columns: [
-                ['EAJ-PNV', 0]
+                ['EAJ-PNV', 0],
+                ['EH Bildu', 0]
             ],
-            type: 'gauge',
+            type : 'donut',
             onclick: function (d, i) { console.log("onclick", d, i); },
             onmouseover: function (d, i) { console.log("onmouseover", d, i); },
             onmouseout: function (d, i) { console.log("onmouseout", d, i); }
         },
-        gauge: {
-            label: {
-                format: function(value, ratio) {
-                    return 75;
-                },
-                show: false // to turn off the min/max labels.
-            },
-            min: 0, // 0 is default, //can handle negative min e.g. vacuum / voltage / current flow / rate of change
-            max: 100, // 100 is default
-            units: ' %',
-            width: 90 // for adjusting arc thickness
-        },
-        color: {
-            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
-            threshold: {
-    //            unit: 'value', // percentage is default
-    //            max: 200, // 100 is default
-                values: [30, 60, 90, 100]
-            }
-        },
-        size: {
-            height: 180
+        donut: {
+            title: "75"
         }
     });
 
     setTimeout(function () {
         chart.load({
-            columns: [['EAJ-PNV', 27]]
+            columns: [
+                ['EAJ-PNV', 27],
+                ['EH Bildu', 22]
+            ]
         });
     }, 1000);
 }());
