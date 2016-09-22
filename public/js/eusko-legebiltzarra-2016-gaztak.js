eb41dab3aa2fe04780c96a526e34e595b228cabd
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-09-22 12:31:13 +0200

    EAEko datuen donuta bistaratzea lortu

--- public/js/eusko-legebiltzarra-2016-gaztak.js
+++ public/js/eusko-legebiltzarra-2016-gaztak.js
@@ -2,28 +2,43 @@
 
     "use strict";
 
-    var chart = c3.generate({
-        data: {
-            columns: [
-                ['EAJ-PNV', 0],
-                ['EH Bildu', 0]
-            ],
-            type : 'donut',
-            onclick: function (d, i) { console.log("onclick", d, i); },
-            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-            onmouseout: function (d, i) { console.log("onmouseout", d, i); }
-        },
-        donut: {
-            title: "75"
-        }
-    });
+    var herrialdeen_json = "datuak/2012/herrialdeak2012.json";
 
-    setTimeout(function () {
-        chart.load({
-            columns: [
-                ['EAJ-PNV', 27],
-                ['EH Bildu', 22]
-            ]
+    d3.json(herrialdeen_json, function(error, emaitzak) {
+
+        if (error) {
+            return console.error(error);
+        }
+
+        var pareak_zero = [];
+        var pareak = [];
+
+        console.log(emaitzak);
+
+        emaitzak.eae.ordena.forEach(function(element, index, array) {
+
+            pareak_zero.push([element, 0]);
+            pareak.push([element, emaitzak.eae.hautagaiak[element].botoak]);
+
         });
-    }, 1000);
+
+        var chart = c3.generate({
+            data: {
+                columns: pareak_zero,
+                type : 'donut',
+                onclick: function (d, i) { console.log("onclick", d, i); },
+                onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+                onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+            },
+            donut: {
+                title: "75"
+            }
+        });
+
+        setTimeout(function () {
+            chart.load({
+                columns: pareak
+            });
+        }, 500);
+    });
 }());
