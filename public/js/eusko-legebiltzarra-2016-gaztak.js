0695a1eaf8036eccfe822461ac9024278158d034
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-09-23 15:44:17 +0200

    Herrialdeen eta EAEko datuen zerrenda bistaratzea lortu

--- public/js/eusko-legebiltzarra-2016-gaztak.js
+++ public/js/eusko-legebiltzarra-2016-gaztak.js
@@ -5,14 +5,79 @@
     var herrialdeen_json1 = "datuak/2012/herrialdeak2012.json";
     var herrialdeen_json2 = "datuak/2016/herrialdeak2016.json";
 
-    var koloreak = {
-        "EH BILDU": "#b3c801",
-        "EAJ-PNV": "#008336",
-        "PSE-EE": "#ed1b24",
-        "PP": "#0BB2FF",
-        "UPYD": "#E4007D",
-        "CIUDADANOS": "#F78934",
-        "PODEMOS": "#6B1F5F"
+    var hautagaiak = {
+        "EH BILDU": {
+            kolorea: "#b3c801",
+            irudia: "img/ehbildu.jpg"
+        },
+        "EAJ-PNV": {
+            kolorea: "#008336",
+            irudia: "img/eaj-pnv.png"
+        },
+        "PSE-EE": {
+            kolorea: "#ed1b24",
+            irudia: "img/pse-ee.gif"
+        },
+        "PP": {
+            kolorea: "#0BB2FF",
+            irudia: "img/pp.png"
+        },
+        "UPyD": {
+            kolorea: "#E4007D",
+            irudia: "img/upyd.png"
+        },
+        "CIUDADANOS": {
+            kolorea: "#F78934",
+            irudia: "img/ciudadanos.jpg"
+        },
+        "PODEMOS": {
+            kolorea: "#6B1F5F",
+            irudia: "img/podemos.png"
+        },
+        "IU-LV": {
+            kolorea: "#4B4B49",
+            irudia: "img/irabazi.png"
+        },
+        "EB-B": {
+            kolorea: "#4B4B49",
+            irudia: "img/irabazi.png"
+        },
+        "EQUO": {
+            irudia: "img/equo.jpeg"
+        },
+        "Eb-Az": {
+            irudia: "img/equo.jpeg"
+        },
+        "PACMA/ATTKA": {
+            irudia: "img/equo.jpeg"
+        },
+        "HARTOS.org": {
+            irudia: "img/equo.jpeg"
+        },
+        "PUM+J": {
+            irudia: "img/equo.jpeg"
+        },
+        "PH": {
+            irudia: "img/equo.jpeg"
+        },
+        "PFyV": {
+            irudia: "img/equo.jpeg"
+        },
+        "POSI": {
+            irudia: "img/equo.jpeg"
+        },
+        "UCE": {
+            irudia: "img/equo.jpeg"
+        },
+        "EK-PCPE": {
+            irudia: "img/equo.jpeg"
+        },
+        "ONGI ETORRI": {
+            irudia: "img/equo.jpeg"
+        },
+        "PYC": {
+            irudia: "img/equo.jpeg"
+        }
     }
 
     d3.json(herrialdeen_json1, function(error, emaitzak1) {
@@ -27,15 +92,69 @@
                 return console.error(error);
             }
 
-            bistaratuDonuta("#eae-donuta", emaitzak2, "eae", 75);
-            bistaratuDonuta("#araba-donuta", emaitzak2, "araba", 25);
-            bistaratuDonuta("#bizkaia-donuta", emaitzak2, "bizkaia", 25);
-            bistaratuDonuta("#gipuzkoa-donuta", emaitzak2, "gipuzkoa", 25);
+            //bistaratuDonuta("#eae-donuta", emaitzak2, "eae", 75);
+            //bistaratuDonuta("#araba-donuta", emaitzak2, "araba", 25);
+            //bistaratuDonuta("#bizkaia-donuta", emaitzak2, "bizkaia", 25);
+            //bistaratuDonuta("#gipuzkoa-donuta", emaitzak2, "gipuzkoa", 25);
 
             bistaratuBarrak("#gipuzkoa-barrak", emaitzak1, emaitzak2, "eae", 5);
 
+            beteTaula("eae", emaitzak1, emaitzak2);
+            beteTaula("araba", emaitzak1, emaitzak2);
+            beteTaula("bizkaia", emaitzak1, emaitzak2);
+            beteTaula("gipuzkoa", emaitzak1, emaitzak2);
         });
     });
+
+    function beteTaula(zer, datuak1, datuak2) {
+
+		var katea = "";
+
+		var aldea = "";
+
+        for (var i = 0; i < datuak2[zer].ordena.length; i++) {
+
+            // Botoak lortu dituzten alderdiak bakarrik bistaratu.
+
+			aldea = "";
+
+			for (var j = 0; j < datuak1[zer].ordena.length; j++) {
+
+				if ((datuak2[zer].hautagaiak[datuak2[zer].ordena[i]]) &&
+                    (datuak2[zer].hautagaiak[datuak2[zer].ordena[i]].izena === datuak1[zer].hautagaiak[datuak1[zer].ordena[j]].izena)) {
+                    console.log(datuak2[zer].hautagaiak[datuak2[zer].ordena[i]]);
+                    console.log(datuak1[zer].hautagaiak[datuak1[zer].ordena[j]]);
+					aldea = datuak2[zer].hautagaiak[datuak2[zer].ordena[i]].hautetsiak - datuak1[zer].hautagaiak[datuak1[zer].ordena[j]].hautetsiak;
+
+				}
+			}
+
+			// Bi hauteskundeetako hautagaiak ez badatoz bat...
+			if (aldea === "") {
+
+				aldea = "-";
+
+			} else if (aldea > 0) {
+
+				aldea = "&#9650;" + aldea;
+
+			} else if (aldea < 0) {
+
+				aldea = "&#9660;" + Math.abs(aldea);
+
+			} else {
+
+				aldea = "=";
+
+			}
+
+			katea = katea + "<tr><td class='irudia'><img src='" +  hautagaiak[datuak2[zer].ordena[i]].irudia + "' /></td><td class='botoak'>" + datuak2[zer].hautagaiak[datuak2[zer].ordena[i]].botoak + "</td><td class='ahulkiak'>" + datuak2[zer].hautagaiak[datuak2[zer].ordena[i]].hautetsiak + "</td><td class='aldea'>" + aldea + "</td></tr>";
+
+        }
+
+        $("#" + zer + " .datuak-taula").html(katea);
+
+	}
 
     function bistaratuBarrak(hautatzailea, emaitzak1, emaitzak2, zer, zenbat_barra_bistaratu) {
 
@@ -63,7 +182,7 @@
 
                     // d will be 'id' when called for legends
                     if (d.index >= 0) {
-                        return koloreak[emaitzak2[zer].ordena[d.index]];
+                        return hautagaiak[emaitzak2[zer].ordena[d.index]].kolorea;
                     }
                     return color;
                 },
@@ -112,6 +231,13 @@
     function bistaratuDonuta(hautatzailea, emaitzak, zer, title) {
         var pareak_zero = [];
         var pareak = [];
+
+        var koloreak = {};
+
+        $.each(hautagaiak, function(index, value) {
+
+            koloreak[index] = value;
+        });
 
         emaitzak[zer].ordena.forEach(function(element, index, array) {
 
